ee90e999e371ea5909147d9f2dedc38171b1de76
Max Schweitzer <max.schweitzer@gmx.at>
2026-06-10 15:09:29 +0200

    feat: add correct answer state close #6

--- Assets/MyGame/Scripts/Wizard.cs
+++ Assets/MyGame/Scripts/Wizard.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,9 +8,20 @@ public class Wizard : MonoBehaviour
     [SerializeField] private int max;
     private int guess;
     [SerializeField] private Text guessText;
+    [SerializeField] private Button correct;
+    [SerializeField] private Button lower;
+    [SerializeField] private Button higher;
     private void Start()
     {
         guess = (min + max) / 2;
         guessText.text = ("I guess... " + guess + "!");
+    }
+
+    public void CorrectGuess()
+    {
+        guessText.text = ("I guessed your number!" + "        Final Guess: " + guess);
+        lower.interactable = false;
+        higher.interactable = false;
+        correct.interactable = false;
     }
 }
